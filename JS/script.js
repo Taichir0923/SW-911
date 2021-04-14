aacfd25f96deb05cb4b1a87e1b3d1725caeede12
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2021-04-14 10:53:18 +0800

    4.14

--- JS/script.js
+++ JS/script.js
@@ -43,22 +43,73 @@
 // })
 
 
-const imgBoxes = document.querySelectorAll('.box');
-const modal = document.querySelector('.modal--container');
-const img = document.querySelector('#img');
+// const imgBoxes = document.querySelectorAll('.box');
+// const modal = document.querySelector('.modal--container');
+// const img = document.querySelector('#img');
 
-imgBoxes.forEach((el, index) => {
-    el.addEventListener('click', function(){
-        img.src = `./images/big/${index}.jpg`;
-        modal.classList.add('open');
-    })
-});
+// imgBoxes.forEach((el, index) => {
+//     el.addEventListener('click', function(){
+//         img.src = `./images/big/${index}.jpg`;
+//         modal.classList.add('open');
+//     })
+// });
 
-modal.addEventListener('click', function(e){
-    if(e.target.classList.contains('open')){
-        e.target.classList.remove('open');
-    }
-})
+// modal.addEventListener('click', function(e){
+//     if(e.target.classList.contains('open')){
+//         e.target.classList.remove('open');
+//     }
+// })
 
 
 // let bla = /$[a-z]/
+
+
+// Create and deleting Dom elements
+
+// append - child element gargaj ogno
+
+// remove - ustgana
+// rgb(0-255,0-255,0-255)
+
+const addBtn = document.querySelector('#addBox');
+const container = document.createElement('div');
+const modalBox = document.querySelector('.modalBox');
+container.className = "container";
+document.body.appendChild(container);
+
+
+
+let boxNum = 0;
+addBtn.onclick = function(){
+    let rNumber = Math.trunc(Math.random() * 256);
+    let gNumber = Math.trunc(Math.random() * 256);
+    let bNumber = Math.trunc(Math.random() * 256);
+    
+    let newBox = document.createElement('div');
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.textContent = 'x';
+    newBox.className = `box box-${boxNum}`;
+    newBox.style.backgroundColor = `rgb(${rNumber}, ${gNumber}, ${bNumber})`;
+    newBox.appendChild(deleteBtn);
+    container.appendChild(newBox);
+    boxNum++;
+
+
+
+    newBox.addEventListener('click', function(e){
+        if(e.target.classList.contains('deleteBtn')){
+            e.target.parentNode.remove();
+        } else {
+            modalBox.children[0].style.backgroundColor = e.target.style.backgroundColor;
+            modalBox.classList.add('open');
+        }
+    })
+}
+
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('open')){
+        modalBox.classList.remove('open');
+    }
+})
+
